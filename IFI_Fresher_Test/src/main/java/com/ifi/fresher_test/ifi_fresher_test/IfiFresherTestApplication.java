34d784a7a84b26905ac2d12879eb4cbd4580b657
Tung Dang Son <dstung@IfiSolution.local>
2021-06-22 16:53:37 +0700

    add IFI_Fresher_Test project

---
+++ IFI_Fresher_Test/src/main/java/com/ifi/fresher_test/ifi_fresher_test/IfiFresherTestApplication.java
@@ -0,0 +1,13 @@
+package com.ifi.fresher_test.ifi_fresher_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfiFresherTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfiFresherTestApplication.class, args);
+    }
+
+}
---
+++ IFI_Fresher_Test/src/test/java/com/ifi/fresher_test/ifi_fresher_test/IfiFresherTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifi.fresher_test.ifi_fresher_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfiFresherTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
